69c3687f458fcc3194ac20f21bb5b06f744a20db
bonniegee <bgordon@rockarch.org>
2020-02-26 15:44:53 -0500

    make nullable fields nullable

--- package_bag/models.py
+++ package_bag/models.py
@@ -8,13 +8,13 @@ from django.contrib.auth.models import AbstractUser
 class Bag(models.Model):
     original_bag_name = models.CharField(max_length=255)
     bag_identifier = models.CharField(max_length=255, unique=True)
-    bag_path = models.CharField(max_length=255, null=True, blank=True)
+    bag_path = models.CharField(max_length=255)
     ORIGIN_CHOICES = (
         ('legacy_digital', 'Legacy Digital Processing'),
         ('digitization', 'Digitization')
     )
-    origin = models.CharField(max_length=20, choices=ORIGIN_CHOICES)
-    rights_id = models.CharField(max_length=255)
+    origin = models.CharField(max_length=20, choices=ORIGIN_CHOICES, null=True, blank=True)
+    rights_id = models.CharField(max_length=255, null=True, blank=True)
     # rights_data = JSONField(null=True, blank=True)
     created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
