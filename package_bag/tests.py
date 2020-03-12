5a8ec727d27a6b751e0f3c49829f3e44dc9ff8a1
bonniegee <bgordon@rockarch.org>
2020-03-12 15:17:46 -0400

    add basic test for each service

--- package_bag/tests.py
+++ package_bag/tests.py
@@ -17,3 +17,18 @@ class TestPackage(TestCase):
         self.assertIsNot(False, discover)
         # make sure the right number of objects were processed
         # make sure that invalid bags were invalidated
+
+    def test_get_rights(self):
+        """docstring for fname"""
+        get_rights = GetRights().run()
+        self.assertIsNot(False, get_rights)
+
+    def test_create_package(self):
+        """docstring for test_create_package"""
+        create_package = CreatePackage().run()
+        self.assertIsNot(False, create_package)
+
+    def test_deliver_package(self):
+        """docstring for test_deliver_package"""
+        deliver_package = DeliverPackage().run()
+        self.assertIsNot(False, deliver_package)
