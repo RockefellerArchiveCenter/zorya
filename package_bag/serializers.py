826f6f77887cc04634c571f9c48ed42549acc5d8
bonniegee <bgordon@rockarch.org>
2020-03-20 17:32:48 -0400

    autopep8

--- package_bag/serializers.py
+++ package_bag/serializers.py
@@ -4,13 +4,14 @@ from .models import Bag
 
 # QUESTION: should I use HyperlinkedModelSerializer instead?
 
+
 class RightsInfoSerializer(serializers.ModelSerializer):
     """docstring for RightsInfoSerializer"""
     # to send to rights service
     class Meta:
         model = Bag
         fields = ("bag_identifier", "rights_id", "end_date")
-        
+
 
 class BagSerializer(serializers.ModelSerializer):
     """docstring for BagSerializer"""
@@ -18,5 +19,3 @@ class BagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bag
         fields = ("bag_identifier", "bag_path", "origin")
-        
-        
