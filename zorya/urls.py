5634213d39dacb778605afe00039da63be442a93
bonniegee <bgordon@rockarch.org>
2020-04-03 15:26:42 -0400

    import views

--- zorya/urls.py
+++ zorya/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from package_bag import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
