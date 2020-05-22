0c69fca919cbf7562f6423dafc6e209c9cbc9013
Hillel Arnold <helrond@hotmail.com>
2020-05-22 13:24:35 -0500

    formatting

--- package_bag/views.py
+++ package_bag/views.py
@@ -2,15 +2,11 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Bag
 from .serializers import BagSerializer
-# Create your views here.
+
 
 class BagViewSet(ModelViewSet):
     """docstring for BagViewSet"""
-    
+
     model = Bag
-    
     queryset = Bag.objects.all()
-    
     serializer_class = BagSerializer
-        
-    pass
