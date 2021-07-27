af6e5cae8943f556073d88648c3bee0a35a3458b
Hillel Arnold <helrond@hotmail.com>
2021-07-27 13:21:56 -0400

    remove as_refid field

--- package_bag/serializers.py
+++ package_bag/serializers.py
@@ -7,8 +7,7 @@ class BagSerializer(serializers.ModelSerializer):
     """Serializer for Digitization Bags"""
 
     identifier = serializers.CharField(source="bag_identifier")
-    as_refid = serializers.CharField(source="original_bag_name")
 
     class Meta:
         model = Bag
-        fields = ("identifier", "origin", "rights_data", "as_refid")
+        fields = ("identifier", "origin", "rights_data")
