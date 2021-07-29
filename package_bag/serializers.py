c0545c326a5e1a19cb5a7be8e75c3f9eda4ea3ac
bonniegee <bgordon@rockarch.org>
2021-07-29 13:10:32 -0400

    add custom serializer method for rights data

--- package_bag/serializers.py
+++ package_bag/serializers.py
@@ -7,7 +7,11 @@ class BagSerializer(serializers.ModelSerializer):
     """Serializer for Digitization Bags"""
 
     identifier = serializers.CharField(source="bag_identifier")
+    rights_statements = serializers.SerializerMethodField(source="rights_data")
 
     class Meta:
         model = Bag
-        fields = ("identifier", "origin", "rights_data")
+        fields = ("identifier", "origin", "rights_statements")
+
+    def get_rights_statements(self, obj):
+        return obj.rights_data['rights_statements']
