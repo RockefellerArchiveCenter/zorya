7a2f9e1283762a5eae1b18fa3edcfe0e84a1a234
bonniegee <bgordon@rockarch.org>
2021-09-27 21:36:26 -0400

    add s3 object download routine

--- package_bag/routines.py
+++ package_bag/routines.py
@@ -6,13 +6,80 @@ from uuid import uuid4
 
 import bagit
 import bagit_profile
+import boto3
 from asterism.bagit_helpers import validate
 from asterism.file_helpers import make_tarfile, remove_file_or_dir
+from botocore.exceptions import ClientError
 from package_bag.serializers import BagSerializer
 from requests import post
 from zorya import settings
 
 from .models import Bag
+
+
+class S3ObjectDownloader(object):
+    def __init__(self, region_name, access_key, secret_key, bucket):
+        s3 = boto3.resource(service_name='s3', region_name=region_name, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+        self.bucket = s3.Bucket(bucket)
+        self.src_dir = settings.SRC_DIR
+
+    def run(self):
+        downloaded = []
+        files_to_download = self.get_list_to_download
+        for file in files_to_download:
+            downloaded_file = self.download_object_from_s3(file)
+            downloaded.append(downloaded_file)
+        msg = "Files downloaded." if len(downloaded) else "No files ready to be downloaded."
+        return msg, downloaded
+
+        # if not in database, download
+
+    def get_list_to_download(self):
+        """Gets list of items to download from S3 bucket, and removes items which do no match criteria
+
+        Returns:
+            List of filenames (strings)"""
+        files_in_bucket = [bucket_object.key for bucket_object in self.bucket.objects.all()]
+        for filename in files_in_bucket:
+            if not self.expected_file_name(filename):
+                files_in_bucket.remove(filename)
+            elif Bag.objects.filter(original_bag_name=join(self.src_dir, filename)):
+                files_in_bucket.remove(filename)
+        return files_in_bucket
+
+    def expected_file_name(self, filename):
+        """Returns true if a filename matches an expected bag name
+
+        Expected bag names are 32 characters before a file extension, and have a file extension of .tar or .tar.gz
+
+        Args:
+            filename (string): a filename (not including full path)
+
+        Returns:
+            boolean: True if filename matches, false otherwise"""
+        filename_split = filename.split(".")
+        if len(filename_split[0]) == 32 and filename_split[-1] in ["tar", "gz"]:
+            return True
+        else:
+            return False
+
+    def download_object_from_s3(self, filename):
+        """Downloads an object from S3 to the source directory
+
+        Args:
+            filename (str): filename which should be the S3 object key as well as the filename to download to
+
+        Returns:
+            downloaded_file (str): full path to downloaded file"""
+        downloaded_file = join(self.src_dir, filename)
+        try:
+            self.bucket.download_file(filename, downloaded_file)
+            return downloaded_file
+        except ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                raise Exception("The object does not exist.")
+            else:
+                raise Exception("Error connecting to AWS: {}".format(e))
 
 
 class BagDiscoverer(object):
@@ -46,7 +113,7 @@ class BagDiscoverer(object):
                 new_bag.save()
             except Exception as e:
                 remove_file_or_dir(bag_path)
-                raise Exception("Error processing discovered bag {}: {}".format(bag, str(e))) from e
+                raise Exception("Error processing discovered bag {}: {}".format(bag, str(e)))
         msg = "Bag discovered, renamed and saved." if bag else "No bags were found."
         return (msg, bag_id) if bag else (msg, None)
 
@@ -99,7 +166,7 @@ class RightsAssigner(object):
                 bags_with_rights.append(bag.bag_identifier)
             except Exception as e:
                 raise Exception(
-                    "Error assigning rights to bag {}: {}".format(bag.bag_identifier, str(e))) from e
+                    "Error assigning rights to bag {}: {}".format(bag.bag_identifier, str(e)))
 
         msg = "Rights assigned." if len(bags_with_rights) else "No bags to assign rights to found."
         return msg, bags_with_rights
@@ -135,7 +202,7 @@ class PackageMaker(object):
                 bag.save()
             except Exception as e:
                 raise Exception(
-                    "Error making package for bag {}: {}".format(bag.bag_identifier, str(e))) from e
+                    "Error making package for bag {}: {}".format(bag.bag_identifier, str(e)))
         msg = "Packages created." if len(packaged) else "No files ready for packaging."
         return msg, packaged
 
