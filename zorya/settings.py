8b5afc2fcbf5dc923aa83c0bf9b5d6f68c7f6a8e
bonniegee <bgordon@rockarch.org>
2021-12-10 16:12:35 -0500

    update example config to prep for cd
    move static path setting in line with our other apps

--- zorya/settings.py
+++ zorya/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = CF.STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Django REST Framework
 REST_FRAMEWORK = {
